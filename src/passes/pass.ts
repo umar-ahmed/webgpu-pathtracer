7c32a0e7b75dfa0b383d6d249b4a2a78ea7d32cf
Umar Ahmed <umar.ahmed1998@gmail.com>
2023-12-28 21:01:20 -0500

    fix casing

---
+++ src/passes/pass.ts
@@ -0,0 +1,12 @@
+import { Renderer } from "../renderer";
+
+export abstract class Pass {
+  protected renderer: Renderer;
+
+  constructor(renderer: Renderer) {
+    this.renderer = renderer;
+  }
+
+  abstract render(commandEncoder: GPUCommandEncoder): void;
+  abstract update(time: number): void;
+}
